9c5cf529dfcc084e6611685d06daf46ca0ee62a5
mathmakgakpak <44949126+mathmakgakpak@users.noreply.github.com>
2019-08-30 18:16:07 +0200

    Initial commit

---
+++ example bot.js
@@ -0,0 +1,13 @@
+const cursorsjs = require("cursorsjs");
+
+const cjs = new cursorsjs.cjs({ws: "ws://159.65.78.102:2828", origin: "http://cursors.io"})
+
+
+cjs.ws.on("open",function(){
+	console.log("Connected".green)
+	cjs.enableConsoleControl()
+	cjs.move(300, 175) // first level 
+})
+cjs.ws.on("close",function(){
+	console.log("Disconnected".red)
+})
---
+++ index.js
@@ -0,0 +1,94 @@
+var WebSocketClient = require('ws');
+const fs = require('fs');
+const colors = require("colors")
+
+class cjs {
+	constructor(options = {}) {
+		let cjs = this
+
+		cjs.ws = new WebSocketClient(options.ws || "ws://159.65.78.102:2828", {
+			headers: {
+				'Origin': options.origin || "http://cursors.io"
+			},
+			agent: options.agent || undefined
+		})
+
+		cjs.enableConsoleControl = function() {
+			let stdin = process.openStdin()
+			stdin.on("data", function(d) {
+				let msg = d.toString().trim();
+
+				try {
+					return console.log(String(eval(msg)))
+				} catch (e) {
+					console.log('[ERROR]:' + e.name + ":" + e.message + "\n" + e.stack)
+				}
+
+			})
+		}
+		cjs.ws.on("message",function(msg){
+
+
+					switch(msg.readUInt8(0)) {
+						case 0:
+						break
+						case 1:
+						break
+						case 4:
+						cjs.position.x = msg.readUInt16LE(1)
+						cjs.position.y = msg.readUInt16LE(3)
+						break
+						case 5:
+						cjs.position.x = msg.readUInt16LE(1)
+						cjs.position.y = msg.readUInt16LE(3)
+						break
+					}
+})
+		cjs.position = {
+			x: 0,
+			y: 0
+		}
+
+		cjs.draw = function(x1, y1, x2, y2) {
+			let array = new ArrayBuffer(9);
+			let dv = new DataView(array);
+
+			dv.setUint8(0, 3);
+			dv.setUint16(1, x1, true);
+			dv.setUint16(3, y1, true);
+			dv.setUint16(5, x2, true);
+			dv.setUint16(7, y2, true);
+			cjs.ws.send(array);
+			cjs.position.y = y2;
+			cjs.position.x = x2;
+		}
+		cjs.move = function(x, y) {
+			let array = new ArrayBuffer(9);
+			let dv = new DataView(array);
+			dv.setUint8(0, 1);
+			dv.setUint16(1, x, true);
+			dv.setUint16(3, y, true);
+			dv.setUint32(5, -1, true);
+			cjs.ws.send(array);
+			cjs.position.x = x;
+			cjs.position.y = y;
+		}
+		cjs.click = function(x, y) {
+			let array = new ArrayBuffer(9);
+			let dv = new DataView(array);
+			dv.setUint8(0, 2);
+			dv.setUint16(1, x, true);
+			dv.setUint16(3, y, true);
+			dv.setUint32(5, -1, true);
+			cjs.ws.send(array);
+			cjs.position.x = x;
+			cjs.position.y = y;
+		}
+
+	}
+
+}
+
+module.exports = {
+	cjs
+}
