8d27d45affa3bcb68665fcc9633ab7c07b045513
mathmakgakpak <44949126+mathmakgakpak@users.noreply.github.com>
2019-08-30 21:42:13 +0200

    update

--- example bot.js
+++ example bot.js
@@ -4,10 +4,10 @@ const cjs = new cursorsjs.cjs({ws: "ws://159.65.78.102:2828", origin: "http://cu
 
 
 cjs.ws.on("open",function(){
-	console.log("Connected".green)
+	console.log("Connected")
 	cjs.enableConsoleControl()
 	cjs.move(300, 175) // first level 
 })
 cjs.ws.on("close",function(){
-	console.log("Disconnected".red)
+	console.log("Disconnected")
 })
