7a54e0350f2bc5b6fa4d035d6b3b0155f1745ae5
mathmakgakpak <44949126+mathmakgakpak@users.noreply.github.com>
2020-03-18 20:17:07 +0100

    move is now promise LuL

--- index.js
+++ index.js
@@ -966,21 +966,21 @@ class cjs extends EventEmitter {
         cjs.position.x = x2;
       }
     }
-    cjs.move = async function(x = cjs.position.x, y = cjs.position.y, pathFinder = true, pathFinderTimeout = 5) {
-
-      if (cjs.ws.readyState == 1) {
+    cjs.move = function(x = cjs.position.x, y = cjs.position.y, pathFinder = true, pathFinderTimeout = 5) {
+      return new Promise(async function(resolve) {
         function move(x, y) {
-          let array = new ArrayBuffer(9);
-          let dv = new DataView(array);
-          dv.setUint8(0, 1);
-          dv.setUint16(1, x, true);
-          dv.setUint16(3, y, true);
-          dv.setUint32(5, -1, true);
-          cjs.ws.send(array);
-          cjs.position.x = x;
-          cjs.position.y = y;
+          if (cjs.ws.readyState == 1) {
+            let array = new ArrayBuffer(9);
+            let dv = new DataView(array);
+            dv.setUint8(0, 1);
+            dv.setUint16(1, x, true);
+            dv.setUint16(3, y, true);
+            dv.setUint32(5, -1, true);
+            cjs.ws.send(array);
+            cjs.position.x = x;
+            cjs.position.y = y;
+          }
         }
-
         if (pathFinder) {
           var moves = zM.path(cjs.position.x, cjs.position.y, x, y, cjs.levelObjects, cjs.grid)
           for (var i = 0; i < moves.length; i++) {
@@ -990,63 +990,61 @@ class cjs extends EventEmitter {
         } else {
           move(x, y)
         }
-      }
+        resolve()
+      })
     }
-
-    cjs.click = function(x = cjs.position.x, y = cjs.position.y) {
-      if (cjs.ws.readyState == 1) {
-        let array = new ArrayBuffer(9);
-        let dv = new DataView(array);
-        dv.setUint8(0, 2);
-        dv.setUint16(1, x, true);
-        dv.setUint16(3, y, true);
-        dv.setUint32(5, -1, true);
-        cjs.ws.send(array);
-        cjs.position.x = x;
-        cjs.position.y = y;
-      }
-    }
-    cjs.drawArray = function(array, x = cjs.position.x, y = cjs.position.y, scale = 1, timeout = 70) {
-      let i = 0;
-
-      function func() {
-        cjs.draw(x + array[i][1] * scale, y + array[i][0] * scale, x + array[i][3] * scale, y + array[i][2] * scale)
-        i++
-        if (i < array.length) setTimeout(func, timeout)
-      }
-      func()
-    }
-    cjs.drawWord = function(str, x = cjs.position.x, y = cjs.position.y, fontSize = 2, kerning = 3, timeout = 250) {
-      str = str.trim()
-      if (str == undefined || str.length <= 0 || cjs.drawing == true) return;
-
-      let i = 0;
-      cjs.drawing = true;
-
-      function func() {
-        var scale = 1
-        if (str.charAt(i) == str.charAt(i).toLowerCase()) scale /= 1.3;
-        let letter = cjs.alphabet[str.toLowerCase().charCodeAt(i)] || cjs.alphabet[63] || []
-
-        for (let line of letter) {
-          let x1 = x + (line[1] * scale + kerning * i) * fontSize;
-          let y1 = y + (line[0] * scale * fontSize);
-          let x2 = x + (line[3] * scale + kerning * i) * fontSize;
-          let y2 = y + (line[2] * scale * fontSize);
-          cjs.draw(x1, y1, x2, y2)
-        }
-        i++
-        if (str.charCodeAt(i)) {
-          setTimeout(func, timeout)
-        } else {
-          cjs.drawing = false;
-          cjs.move(x, y)
-        }
-      }
-      func()
-    }
-
   }
+
+  cjs.click = function(x = cjs.position.x, y = cjs.position.y) {
+    if (cjs.ws.readyState == 1) {
+      let array = new ArrayBuffer(9);
+      let dv = new DataView(array);
+      dv.setUint8(0, 2);
+      dv.setUint16(1, x, true);
+      dv.setUint16(3, y, true);
+      dv.setUint32(5, -1, true);
+      cjs.ws.send(array);
+      cjs.position.x = x;
+      cjs.position.y = y;
+    }
+  }
+  cjs.drawArray = async function(array, x = cjs.position.x, y = cjs.position.y, scale = 1, timeout = 70) {
+    for (var i = 0; i < array.length; i++) {
+      cjs.draw(x + array[i][1] * scale, y + array[i][0] * scale, x + array[i][3] * scale, y + array[i][2] * scale)
+      await sleep(timeout)
+    }
+  }
+  cjs.drawWord = function(str, x = cjs.position.x, y = cjs.position.y, fontSize = 2, kerning = 3, timeout = 250) {
+    str = str.trim()
+    if (str == undefined || str.length <= 0 || cjs.drawing == true) return;
+
+    let i = 0;
+    cjs.drawing = true;
+
+    function func() {
+      var scale = 1
+      if (str.charAt(i) == str.charAt(i).toLowerCase()) scale /= 1.3;
+      let letter = cjs.alphabet[str.toLowerCase().charCodeAt(i)] || cjs.alphabet[63] || []
+
+      for (let line of letter) {
+        let x1 = x + (line[1] * scale + kerning * i) * fontSize;
+        let y1 = y + (line[0] * scale * fontSize);
+        let x2 = x + (line[3] * scale + kerning * i) * fontSize;
+        let y2 = y + (line[2] * scale * fontSize);
+        cjs.draw(x1, y1, x2, y2)
+      }
+      i++
+      if (str.charCodeAt(i)) {
+        setTimeout(func, timeout)
+      } else {
+        cjs.drawing = false;
+        cjs.move(x, y)
+      }
+    }
+    func()
+  }
+
+}
 
 }
 
